11e137c3f706bc833d356defa188b637ec3afc86
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2026-06-20 15:42:35 +0200

    Bump split barrier jobs to 2h wall time

--- exercise1/scripts/barrier_latencies_alg24.sh
+++ exercise1/scripts/barrier_latencies_alg24.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=128
 #SBATCH --output=../logs/output_epyc_alg24.%j.out
 #SBATCH --error=../logs/error_epyc_alg24.%j.err
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --partition=EPYC
 #SBATCH --exclusive
 
